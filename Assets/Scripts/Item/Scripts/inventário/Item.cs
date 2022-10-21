2c80daa8f31608b2399c9b3fdaabd0c4b84c53d3
NaoeoBryan <bryanppfranca@gmail.com>
2022-10-21 18:33:51 -0300

    Alpha_1.2
    Quase no beta

---
+++ Assets/Scripts/Item/Scripts/inventário/Item.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item 
+{
+    public string id;
+    public int iDefesa;
+    public int iAtaque;
+    public int iDestreza;
+    public int iVida;
+    public Sprite iImage;
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# Version Alfa
-
-<p>Versão de demonstração do jogo em desenvolvimento The Creed Of Gods<p>
-
-
